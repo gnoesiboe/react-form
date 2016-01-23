6ed6ca5d65765bc385285dc2bf360007627b012a
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2016-01-23 16:57:27 +0100

    add statuses to form groups

--- src/js/lib/form/component/formGroupComponent.js
+++ src/js/lib/form/component/formGroupComponent.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import * as componentChildHelper from './../helper/componentChildHelper';
 
+const STATUS_ERROR = 'error';
+const STATUS_SUCCESS = 'success';
+const STATUS_PENDING = 'pending';
+
 /**
  * @author Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
  */
 class FormGroupComponent extends React.Component {
 
+    /**
+     * @param {Object} props
+     */
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            status: STATUS_PENDING
+        };
+    }
+
+    /**
+     * @param {Array} children
+     *
+     * @returns {Array}
+     *
+     * @private
+     */
     _augmentChildrenFormElements(children) {
         return componentChildHelper.cloneChildFormElements(
             children,
@@ -48,13 +70,39 @@ class FormGroupComponent extends React.Component {
     }
 
     /**
+     * @returns {String}
+     *
+     * @private
+     */
+    _defineClassName() {
+        var out = ['form-group'];
+
+        switch (this.state.status) {
+            case STATUS_ERROR:
+                out.push('has-error');
+                out.push('has-feedback');
+                break;
+
+            case STATUS_SUCCESS:
+                out.push('has-success');
+                out.push('has-feedback');
+                break;
+
+            default:
+                break;
+        }
+
+        return out.join(' ');
+    }
+
+    /**
      * @returns {XML}
      */
     render() {
         var children = this._augmentChildrenFormElements(this.props.children);
 
         return (
-            <div className="form-group has-success has-feedback">
+            <div className={this._defineClassName()}>
                 {children}
             </div>
         );
